d99c9e4f6029483cddcbf3c8b3ce6760d3614954
Chris Witte <christopher.t.witte@gmail.com>
2022-08-08 17:52:52 -0400

    commit tree 31e6173156457b1e26de3d563ecb6280c2756927

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    const quote = `This Saturday, I want to ${activity}!`;
+    return quote
+}
+
+function mondayWork (activity = 'go to the office') {
+    const quote = `This Monday, I will ${activity}.`;
+    return quote
+}
+
+function wrapAdjective (highlight) {
+    const inner = (adjective = "special") => `You are ${highlight}${adjective}${highlight}!`
+    return inner
+}
